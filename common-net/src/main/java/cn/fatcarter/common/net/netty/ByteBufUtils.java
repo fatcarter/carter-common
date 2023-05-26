a9a60a9f2845af1b3da0d86ca004edb28f464e1d
michaelzhang055 <michaelzhang055@hotmail.com>
2023-05-26 13:54:34 +0800

    feat: add  method

--- common-net/src/main/java/cn/fatcarter/common/net/netty/ByteBufUtils.java
+++ common-net/src/main/java/cn/fatcarter/common/net/netty/ByteBufUtils.java
@@ -1,5 +1,6 @@
 package cn.fatcarter.common.net.netty;
 
+import cn.fatcarter.common.net.ByteUtils;
 import io.netty.buffer.ByteBuf;
 
 public class ByteBufUtils {
@@ -11,5 +12,10 @@ public class ByteBufUtils {
         byte[] bytes = new byte[len];
         buf.readBytes(bytes);
         return bytes;
+    }
+
+    public static String readBytesHex(ByteBuf buf, int len) {
+        byte[] bytes = readBytes(buf, len);
+        return ByteUtils.toHex(bytes);
     }
 }
