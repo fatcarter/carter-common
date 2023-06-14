eedac03514fcd52932633336461dfd6a792b8756
michaelzhang055 <michaelzhang055@hotmail.com>
2023-06-14 15:08:33 +0800

    feat: add  method

--- common-core/src/main/java/cn/fatcarter/common/util/ListUtils.java
+++ common-core/src/main/java/cn/fatcarter/common/util/ListUtils.java
@@ -2,7 +2,9 @@ package cn.fatcarter.common.util;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.function.Function;
 
 public class ListUtils {
@@ -23,4 +25,27 @@ public class ListUtils {
     public static <T> List<T> subtract(Collection<T> source, Collection<T> willBeRemoved) {
         return subtract(source, willBeRemoved, ArrayList::new);
     }
+
+    public static <T> List<T> distinct(Collection<T> collect) {
+        return distinct(collect, ArrayList::new);
+    }
+
+    public static <T> List<T> distinct(Collection<T> collect, Function<Collection<T>, List<T>> creator) {
+        if (collect instanceof Set) {
+            return creator.apply(collect);
+        }
+        Set<T> set = new HashSet<>(collect);
+        return creator.apply(set);
+    }
+
+    public static <T> List<T> merge(Collection<T> one, Collection<T> two) {
+        return merge(one, two, ArrayList::new);
+    }
+
+    public static <T> List<T> merge(Collection<T> one, Collection<T> two, Function<Collection<T>, List<T>> creator) {
+        List<T> res = creator.apply(one);
+        res.addAll(two);
+        return res;
+    }
+
 }
