01d70830c0b8e08eefa836ea2808dbf0fb7a2add
michaelzhang055 <michaelzhang055@hotmail.com>
2023-07-27 10:04:25 +0800

    add HttpServletRequestWrapper

---
+++ common-core/src/main/java/cn/fatcarter/common/servlet/http/jakarta/ReadableHttpServletRequestWrapper.java
@@ -0,0 +1,80 @@
+package cn.fatcarter.common.servlet.http.jakarta;
+
+import jakarta.servlet.ReadListener;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class ReadableHttpServletRequestWrapper extends HttpServletRequestWrapper {
+    private ServletInputStream stream;
+    private Set<String> excludes = new HashSet<>();
+
+
+    public ReadableHttpServletRequestWrapper(HttpServletRequest request) {
+        super(request);
+    }
+
+    public ReadableHttpServletRequestWrapper excludeUrl(String url) {
+        this.excludes.add(url);
+        return this;
+    }
+
+    public ReadableHttpServletRequestWrapper excludeUrls(String... url) {
+        this.excludes.addAll(Arrays.asList(url));
+        return this;
+    }
+
+    @Override
+    public ServletInputStream getInputStream() throws IOException {
+        if (stream == null) {
+            stream = getReadableInputStream();
+        }
+        return stream;
+    }
+
+    protected ServletInputStream getReadableInputStream() throws IOException {
+        return new ReadableServletInputStream(super.getInputStream());
+    }
+
+    private static class ReadableServletInputStream extends ServletInputStream {
+        private final ByteArrayInputStream stream;
+
+        public ReadableServletInputStream(ServletInputStream stream) throws IOException {
+            byte[] buffer = new byte[1024];
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            int len;
+            while ((len = stream.read(buffer)) != -1) {
+                out.write(buffer, 0, len);
+            }
+            this.stream = new ByteArrayInputStream(out.toByteArray());
+        }
+
+        @Override
+        public boolean isFinished() {
+            return false;
+        }
+
+        @Override
+        public boolean isReady() {
+            return true;
+        }
+
+        @Override
+        public void setReadListener(ReadListener readListener) {
+
+        }
+
+        @Override
+        public int read() throws IOException {
+            return stream.read();
+        }
+    }
+}
