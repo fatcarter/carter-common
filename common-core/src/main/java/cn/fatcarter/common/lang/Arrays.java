e04317a0f2f5bbe6d58eec3fbe5b7c0d852809bc
zhangyujia <zhangyujia@pinming.cn>
2024-09-09 14:35:13 +0800

    feat: 增加工具方法

--- common-core/src/main/java/cn/fatcarter/common/lang/Arrays.java
+++ common-core/src/main/java/cn/fatcarter/common/lang/Arrays.java
@@ -2,6 +2,7 @@ package cn.fatcarter.common.lang;
 
 import cn.fatcarter.common.util.ArrayUtils;
 import cn.fatcarter.common.util.Assert;
+import cn.fatcarter.common.util.Op;
 
 import java.lang.reflect.Array;
 
@@ -11,6 +12,10 @@ public class Arrays {
         T[] o = (T[]) Array.newInstance(t[0].getClass(), t.length);
         System.arraycopy(t, 0, o, 0, t.length);
         return o;
+    }
+
+    public static String[] split(String src, String delimiter){
+        return Op.notBlank(src).map(s -> s.split(delimiter)).orElse(new String[0]);
     }
 
     public static <T> T[] newInstance(Class<T> clz,int size) {
