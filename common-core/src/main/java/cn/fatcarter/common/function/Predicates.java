34c619f688ecf3b4614845cd61976612f2fceca9
zhangyujia <zhangyujia@pinming.cn>
2025-04-21 16:23:29 +0800

    feat: 添加工具类

--- common-core/src/main/java/cn/fatcarter/common/function/Predicates.java
+++ common-core/src/main/java/cn/fatcarter/common/function/Predicates.java
@@ -1,5 +1,9 @@
 package cn.fatcarter.common.function;
 
+import cn.fatcarter.common.util.StringUtils;
+
+import java.util.Objects;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public final class Predicates {
@@ -9,5 +13,17 @@ public final class Predicates {
 
     public static <T> Predicate<T> not(Predicate<T> predicate) {
         return predicate.negate();
+    }
+
+    public static <T, R> Predicate<T> notNull(Function<T, R> mapper) {
+        return mapper(mapper, Objects::nonNull);
+    }
+
+    public static <T> Predicate<T> notBlank(Function<T, String> mapper) {
+        return mapper(mapper, StringUtils::isNotBlank);
+    }
+
+    public static <T, R> Predicate<T> mapper(Function<T, R> mapper, Predicate<R> predicate) {
+        return t -> predicate.test(mapper.apply(t));
     }
 }
