3f1ad1f2022bcfd187ac27f19c9df04ccb2f9a43
zhangyujia <zhangyujia@pinming.cn>
2025-08-06 09:25:57 +0800

    ACCEPTED 支持list

--- common-validation/src/main/java/cn/fatcarter/common/validation/ValidatorSupport.java
+++ common-validation/src/main/java/cn/fatcarter/common/validation/ValidatorSupport.java
@@ -1,13 +1,20 @@
 package cn.fatcarter.common.validation;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
 public class ValidatorSupport {
 
     protected boolean accept(Object value, List<String> acceptValues) {
-        if (value instanceof Map) {
-            Map<?, ?> map = (Map<?, ?>) value;
+        if (value == null) return true;
+        if (value instanceof Collection<?> coll) {
+            for (Object o : coll) {
+                boolean accept = accept(o, acceptValues);
+                if(!accept) return false;
+            }
+        }
+        if (value instanceof Map<?,?> map) {
             for (Object o : map.keySet()) {
                 if (!(o instanceof String) && o != null) {
                     o = String.valueOf(o);
@@ -19,7 +26,7 @@ public class ValidatorSupport {
             }
             return true;
         }
-        if (value == null) return true;
+
         if (!(value instanceof String)) {
             value = String.valueOf(value);
         }
